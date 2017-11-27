c492c1db6d87731af22b1e5700f19498040cdc9a
Your Name <robpeterman573@gmail.com>
2017-11-26 20:15:44 -0500

    basic endpoints + server.js tests

--- server.js
+++ server.js
@@ -2,13 +2,158 @@
 
 const express = require('express');
 const app = express();
+const {DATABASE_URL, PORT} = require('./config');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const {List} = require('./models');
+require('dotenv').config();
 
-const PORT = process.env.PORT || 3000;
+mongoose.promise = global.promise;
+app.use(bodyParser.json());
+const cors = require('cors');
 
-app.get('/api/*', (req, res) => {
-  res.json({ok: 'yas'});
+const {CLIENT_ORIGIN} = require('./config');
+
+app.use(cors({origin: CLIENT_ORIGIN}));
+
+// retrive array of all list names for user
+app.get('/list-state/name-list', (req, res)=>{
+  let nameArray = [];
+  List
+    .find({})
+    .exec()
+    .then((listState)=>{
+      for(let i = 0; i< listState.length; i++){
+        nameArray.push(listState[i].listName);
+      }
+      res.json(nameArray);
+    })
+    .catch(
+      err => {
+        console.error(err);
+        res.status(500).json({message: 'Internal server error'});
+      });
+});
+// retrieve individual list after user click on a name from the list
+app.get('/list-state/:name', (req, res)=>{
+  List
+    .findOne({listName: req.params.name})
+    .then((state)=>{
+      // if(state === null){
+      //   res.status(404)
+      //     .end();
+      // }
+      res.json(state);
+
+    })
+    .catch(
+      err => {
+        console.error(err);
+        res.status(500).json({message: 'Internal server error'});
+      });
+});
+// for initializing the state of a list server side after user names a list
+app.post('/list-state', (req, res)=>{
+  List
+    .create({     	
+      listName: req.body.listName,
+      totalWeight: 0,
+      totalItems: 0,
+      weightGoal: 0,
+      hiking: [],
+      clothing: [],
+      navigation: [],
+      shelter: [],
+      sleep:[],
+      cooking:[],
+      water:[],
+      hygiene:[],
+      firstAid: [],
+      misc: []
+    })
+    .then(function(post) {
+      res.status(201).json(post);
+  
+    });
+});
+// for updating state on 'save' presses 
+app.put('/list-state/:listname', (req,res)=>{
+  List
+    .findOneAndUpdate({listName: req.params.listname}, 
+      
+      {
+        'totalWeight': 0,
+        'totalItems': 0,
+        'weightGoal': req.body.weightGoal,
+        'misc': req.body.misc,
+        'firstAid': req.body.firstAid,
+        'hygiene': req.body.hygiene,
+        'water': req.body.water,
+        'cooking': req.body.cooking,
+        'sleep': req.body.sleep,
+        'shelter': req.body.shelter,
+        'navigation': req.body.navigation,
+        'clothing': req.body.clothing,
+        'hiking': req.body.hiking
+      },
+      
+      {new: true})
+    
+    .then(function(updatedItem) {
+      res.status(201).json(updatedItem)
+        .catch(function(err) {
+          res.status(500).json({
+            message: 'Something went wrong'
+          });
+        });
+    });
+});
+app.delete('/list-state/:deletename', (req, res)=>{
+  List.
+    findOneAndRemove({listName: req.params.deletename})
+    .then(function(){
+      res.status(204).end();
+    });
 });
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+let server;
+function runServer(databaseUrl = DATABASE_URL, port = PORT) {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+        .on('error', err => {
+          mongoose.disconnect();
+          reject(err);
+        });
+    });
+  });
+}
 
-module.exports = {app};
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
+}
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+}
+module.exports = {
+  app,
+  runServer,
+  closeServer
+};
+
--- test/test-server.js
+++ test/test-server.js
@@ -1,19 +1,173 @@
 'use strict';
-
+const {app, runServer, closeServer} = require('../server.js');
 const chai = require('chai');
-const chaiHttp = require('chai-http');
-const {app} = require('../server');
+const chaiHttp = require('chai-Http');
 const should = chai.should();
+const {List} = require('../models');
+const faker = require('faker');
+const mongoose = require('mongoose');
+const {TEST_DATABASE_URL} = require('../config');
+const keyList = ['misc', 'firstAid', 'hygiene', 'water', 'cooking', 'sleep', 'shelter', 'navigation', 'clothing', 'hiking'];
+mongoose.promise = global.promise;
 chai.use(chaiHttp);
 
-describe('API', function() {
+function seedData(){
+  let dataArray = [];
+  for(let i=0; i<10; i++){
+    dataArray.push(generateData());
+  }
+  return List.insertMany(dataArray);
+}
+function randomNumber(){
+  const numberArray = [5, 22, 43, 75, 234, 121, 15, 44, 94, 55, 230, 34];
+  return numberArray[Math.floor(Math.random() * numberArray.length)];
+}
+function generateData(){
+  return {
+    'listName': faker.hacker.verb(),
+    'totalWeight': 0,
+    'totalItems': 0,
+    'weightGoal': randomNumber(),
+    'misc': [{'name': faker.hacker.verb(), 'weight': randomNumber()},
+      {'name': faker.hacker.verb(), 'weight': randomNumber()},
+      {'name': faker.hacker.verb(), 'weight': randomNumber()},
+      {'name': faker.hacker.verb(), 'weight': randomNumber()}
+    ],
+    'firstAid': [{'name': faker.hacker.verb(), 'weight': randomNumber()},
+      {'name': faker.hacker.verb(), 'weight': randomNumber()},
+      {'name': faker.hacker.verb(), 'weight': randomNumber()},
+      {'name': faker.hacker.verb(), 'weight': randomNumber()}
+    ],
+    'hygiene': [{'name': faker.hacker.verb(), 'weight': randomNumber()},
+      {'name': faker.hacker.verb(), 'weight': randomNumber()},
+      {'name': faker.hacker.verb(), 'weight': randomNumber()},
+      {'name': faker.hacker.verb(), 'weight': randomNumber()}
+    ],
+    'water': [],
+    'cooking': [{'name': faker.hacker.verb(), 'weight': randomNumber()},
+      {'name': faker.hacker.verb(), 'weight': randomNumber()},
+      {'name': faker.hacker.verb(), 'weight': randomNumber()},
+    ],
+    'sleep': [],
+    'shelter': [{'name': faker.hacker.verb(), 'weight': randomNumber()},
+      {'name': faker.hacker.verb(), 'weight': randomNumber()},
+      {'name': faker.hacker.verb(), 'weight': randomNumber()},
+      {'name': faker.hacker.verb(), 'weight': randomNumber()}
+    ],
+    'navigation': [],
+    'clothing': [{'name': faker.hacker.verb(), 'weight': randomNumber()},
+    ],
+    'hiking': []
+  };
+}
+function dropDB(){
+    
+  return new Promise(function(resolve, reject){
+    mongoose.connection.dropDatabase()
+      .then(function(result){
+        resolve(result);
+      })
+      .catch(function(err){
+        reject(err);
+      });
+  });
+}
 
-  it('should 200 on GET requests', function() {
+describe('all API endpoints', function(){
+  
+  before(function(){
+    return runServer();
+  });
+  
+  beforeEach(function(){
+    return seedData();
+  });
+  
+  afterEach(function(){
+    return dropDB();
+  });
+  after(function(){
+    return closeServer();
+  });
+  
+  it('should return 200 on get requests to /list-state/name-list', function() {
     return chai.request(app)
-      .get('/api/fooooo')
+      .get('/list-state/name-list')
       .then(function(res) {
         res.should.have.status(200);
+      });
+  });
+    
+  it('should return 200 on get requests to /list-state/:name', function(){
+    let searchName;
+    return chai.request(app)
+      .get('/list-state/name-list')
+      .then(function(res){
+        searchName = res.body[1];
+        return chai.request(app)
+          .get(`/list-state/${searchName}`)
+          .then(function(res){
+            res.should.be.json;
+            res.should.have.status(200);
+          });
+      });
+  });
+
+  it('should successfully create a new list on post requests to /list-state', function(){
+
+    const newItem = generateData();
+    return chai.request(app)
+      .post('/list-state')
+      .send(newItem)
+      .then(function(res) {
+        res.should.have.status(201);
         res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.should.include.keys(keyList);
+      });
+  });
+
+  it('should successfully update the state on the server with put requests to /list-state', function(){
+    let resultName;
+    let originalData;
+    let updateData = generateData();
+    delete updateData.listName;
+    delete updateData.totalItems;
+    delete updateData.totalWeight;
+    return chai.request(app)
+      .get('/list-state/name-list')
+      .then(function(res){
+        originalData = res.body;
+        resultName = res.body[0];
+        return chai.request(app)
+          .put(`/list-state/${resultName}`)
+          .send(updateData)
+          .then(function(res){
+            res.should.be.json;
+            res.body.should.be.a('object');
+            res.body.should.include.keys(keyList);
+            res.body.should.not.equal(originalData);
+          });
+      });
+
+  });
+  it('should succesfully delete a list on delete requests to list-state/:deletename', function(req, res){
+    let resultName;
+    return chai.request(app)
+      .get('/list-state/name-list')
+      .then(function(res){
+        resultName = res.body[0];
+        return chai.request(app)
+          .delete(`/list-state/${resultName}`)
+          .then(function(res){
+            res.should.have.status(204);
+            return chai.request(app)
+              .get(`/list-state/${resultName}`)
+              .then(function(res){
+                res.should.have.status(404);
+              });
+          });
       });
   });
 });
+
