32668a6baf458a0dbbd032c606133e6bc26b2060
Your Name <robpeterman573@gmail.com>
2017-11-26 20:13:35 -0500

    updated database url + added test database url and client origin

--- config.js
+++ config.js
@@ -1,7 +1,16 @@
-
-
 'use strict';
 
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                    //   'mongodb://localhost/remote-working-locations';
+                       'mongodb://localhost/ul-packing-list';
+
+
+exports.TEST_DATABASE_URL = (
+  process.env.TEST_DATABASE_URL ||
+    'mongodb://localhost/test-List');
+
+exports.PORT = process.env.PORT || 8080;
+    
+                       
+exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
+
