9e5bd2c6f87215cb6cc23cc08731ba2f728c5636
Your Name <robpeterman573@gmail.com>
2017-11-26 20:14:29 -0500

    changed model to only include empty arrays + capitalized List

--- models.js
+++ models.js
@@ -1,26 +1,24 @@
 'use strict';
 const mongoose = require('mongoose');
+const packingListState = mongoose.Schema(
+  {
+    listName: String,
+    totalWeight: Number,
+    totalItems: Number,
+    weightGoal: Number,
 
-mongoose.Promise = global.Promise;
-
-const packingListState = mongoose.Schema({
-    
-  totalWeight: Number,
-  totalItems: Number,
-  weightGoal: Number,
-
-  hiking: [{name: String, weight: Number}],
-  clothing: [{name: String, weight: Number}],
-  navigation: [{name: String, weight: Number}],
-  shelter: [{name: String, weight: Number}],
-  sleep:[{name: String, weight: Number}],
-  cooking:[{name: String, weight: Number}],
-  water: [{name: String, weight: Number}],
-  hygiene: [{name: String, weight: Number}],
-  firstAid: [{name: String, weight: Number}],
-  misc: [{name: String, weight: Number}]
-}
+    hiking: [],
+    clothing: [],
+    navigation: [],
+    shelter: [],
+    sleep:[],
+    cooking:[],
+    water: [],
+    hygiene: [],
+    firstAid: [],
+    misc: []
+  }
 );
 
-const list = mongoose.model('list', packingListState);
-module.exports = {list};
+const List = mongoose.model('List', packingListState);
+module.exports = {List};
