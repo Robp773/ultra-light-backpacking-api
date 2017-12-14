c24fa8d25e441f50810db1ab8042d8e2710aa406
Your Name <robpeterman573@gmail.com>
2017-12-14 11:25:32 -0500

    neatened up code

--- config.js
+++ config.js
@@ -1,16 +1,10 @@
 'use strict';
 
-exports.DATABASE_URL = process.env.DATABASE_URL ||
-                       global.DATABASE_URL ||
-                       'mongodb://localhost/ul-packing-list';
+exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://localhost/ul-packing-list';
 
-
-exports.TEST_DATABASE_URL = (
-  process.env.TEST_DATABASE_URL ||
-    'mongodb://localhost/test-List');
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-List';
 
 exports.PORT = process.env.PORT || 8080;
-    
                        
 exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
-
+ 
