81d5a870cae9e5376e2322aa75e6a978478a3df0
Your Name <robpeterman573@gmail.com>
2017-12-18 14:04:07 -0500

    removed localhost element from DATABASE_URL

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 'use strict';
 
-exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://localhost:27017/ul-packing-list';
+exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://localhost/ul-packing-list';
+
+
 
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-List';
 
